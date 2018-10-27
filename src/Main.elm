960416328d2366dd5e2aff220b3d1882de8e0978
Benjamin Ortiz <hello.ben.ortiz@gmail.com>
2018-10-27 10:25:50 -0700

    Remove localstorage support and add basic location types

--- src/Main.elm
+++ src/Main.elm
@@ -1,26 +1,27 @@
 module Game exposing (main)
 
 import Browser
-import Json.Decode as Decode
+import Json.Encode exposing (Value)
 import Messages exposing (Msg(..))
-import Model exposing (Model, decode, initial)
+import Model exposing (Model, initial)
 import Update exposing (update)
 import View exposing (view)
 
 
 main =
     Browser.element
-        { init =
-            \value ->
-                ( value
-                    |> Decode.decodeValue Model.decode
-                    |> Result.withDefault Model.initial
-                , Cmd.none
-                )
+        { init = init
         , update = update
         , subscriptions = subscriptions
         , view = view
         }
+
+
+init : Value -> ( Model, Cmd msg )
+init flags =
+    ( Model.initial
+    , Cmd.none
+    )
 
 
 subscriptions : Model -> Sub Msg
--- src/Update.elm
+++ src/Update.elm
@@ -1,19 +1,20 @@
 port module Update exposing (update)
 
 import Messages exposing (Msg(..))
-import Model exposing (Model, encode)
+import Model exposing (Model)
 
 
 port save : String -> Cmd msg
 
 
-saveToStorage : Model -> ( Model, Cmd Msg )
-saveToStorage model =
-    ( model, save (Model.encode 2 model) )
+
+-- saveToStorage : Model -> ( Model, Cmd Msg )
+-- saveToStorage model =
+--     ( model, save (Model.encode 2 model) )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         SpendPoints points ->
-            saveToStorage { model | actionPoints = model.actionPoints - points }
+            ( { model | actionPoints = model.actionPoints - points }, Cmd.none )
